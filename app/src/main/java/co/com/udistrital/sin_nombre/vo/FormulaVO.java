753e466f4bb9e590ed588bb9d47cd05c417f922e
Nerfo1124 <dfernando1124@hotmail.com>
2016-01-17 00:48:28 -0500

    Sin_Nombre: Se agregan ultimos atributos de las clases para el objeto auxiliar.

--- app/src/main/java/co/com/udistrital/sin_nombre/vo/FormulaVO.java
+++ app/src/main/java/co/com/udistrital/sin_nombre/vo/FormulaVO.java
@@ -3,7 +3,7 @@ package co.com.udistrital.sin_nombre.vo;
 import java.util.Date;
 
 /**
- * Created by Fernando on 16/01/2016.
+ * Created by Fernando Hernandez on 16/01/2016.
  */
 public class FormulaVO {
 
@@ -34,6 +34,24 @@ public class FormulaVO {
         return sexo;
     }
 
-
     // Bloque Setter
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void setApellido2Usuario(String apellido2Usuario) {
+        this.apellido2Usuario = apellido2Usuario;
+    }
+
+    public void setApellido1Usuario(String apellido1Usuario) {
+        this.apellido1Usuario = apellido1Usuario;
+    }
 }
--- app/src/main/java/co/com/udistrital/sin_nombre/vo/SesionVO.java
+++ app/src/main/java/co/com/udistrital/sin_nombre/vo/SesionVO.java
@@ -1,7 +1,7 @@
 package co.com.udistrital.sin_nombre.vo;
 
 /**
- * Created by Fernando on 16/01/2016.
+ * Created by Fernando Hernandez on 16/01/2016.
  */
 public class SesionVO {
 
