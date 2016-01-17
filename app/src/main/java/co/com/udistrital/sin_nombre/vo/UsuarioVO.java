daebabaea84bb3b5b27cad186359dca296458751
Nerfo1124 <dfernando1124@hotmail.com>
2016-01-16 23:06:46 -0500

    Sin_Nombre: Se crean primeros paquetes de la estructura final del proyecto contra la presbicia y se agregan objetos auxiliares para manejo de datos solicitados en la aplicacion

---
+++ app/src/main/java/co/com/udistrital/sin_nombre/vo/UsuarioVO.java
@@ -0,0 +1,15 @@
+package co.com.udistrital.sin_nombre.vo;
+
+import java.util.Date;
+
+/**
+ * Created by Fernando on 16/01/2016.
+ */
+public class UsuarioVO {
+
+    private String nombreUsuario;
+    private String apellido1Usuario;
+    private String apellido2Usuario;
+    private Date fechaNacimiento;
+    private String sexo;
+}
