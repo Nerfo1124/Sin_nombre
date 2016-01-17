25effbcc489f052c93fa7c6f8c1f5bec4616c041
Nerfo1124 <dfernando1124@hotmail.com>
2016-01-16 22:30:34 -0500

    Sin_Nombre: Se crean primeros paquetes de la estructura final del proyecto contra la presbicia.

--- app/src/main/java/co/com/udistrital/sin_nombre/view/inicio_sesion.java
+++ app/src/main/java/co/com/udistrital/sin_nombre/view/inicio_sesion.java
@@ -1,7 +1,9 @@
-package co.com.udistrital.sin_nombre;
+package co.com.udistrital.sin_nombre.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import co.com.udistrital.sin_nombre.R;
 
 public class inicio_sesion extends AppCompatActivity {
 
