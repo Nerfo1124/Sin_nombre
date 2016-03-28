fd66720ff73c90a6e4275762c6ca28ea932188ae
Nerfo1124 <dfernando1124@hotmail.com>
2016-03-27 21:32:04 -0500

    Sin_nombre: Ajuste sobre el metodo de carga de los ejercicios desde Base de Datos.

--- app/src/main/java/co/com/udistrital/sin_nombre/view/Ejercicios.java
+++ app/src/main/java/co/com/udistrital/sin_nombre/view/Ejercicios.java
@@ -32,6 +32,7 @@ public class Ejercicios extends AppCompatActivity implements AdapterView.OnItemC
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(LOG_TAG, "Ingresando a la vista Ejercicios.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicios);
         /*NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
@@ -42,63 +43,18 @@ public class Ejercicios extends AppCompatActivity implements AdapterView.OnItemC
         ejerciciosView = (ListView) findViewById(R.id.listaEjercicios);
         ejerciciosView.setOnItemClickListener(this);
 
-        EjercicioProccesor mytask = new EjercicioProccesor();
-        mytask.execute("listExcercises.txt");
+        try {
+            EjercicioDAO dao = new EjercicioDAO(mContext);
+            ejercicioInfoList = dao.list();
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Error durante la carga de los ejercicios.", e);
+        }
+
+        myArrayAdapter = new ArrayAdapter(mContext, ejercicioInfoList);
+        ejerciciosView.setAdapter((ListAdapter) myArrayAdapter);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    }
-
-    private class EjercicioProccesor extends AsyncTask<String, Void, Integer> {
-        ProgressDialog progressDialog;
-        int delay = 5000; // ms
-
-        public EjercicioProccesor() {
-            super();
-        }
-
-        // Se muestra un mensaje mientras en segundo plano se ejecuta la carga de los Ejercicios.
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressDialog = ProgressDialog.show(mContext, "Por favor espere!", "Cargando lista de ejercicios...");
-        }
-
-        // Este metodo se ejecuta en segundo plano y se encarga de cargar los Ejercicios.
-        @Override
-        protected Integer doInBackground(String... strings) {
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                Log.e(LOG_TAG, "Error durante la ejecucion del metodo.");
-            }
-            // Carga de la lista de ejercicios.
-            try {
-                EjercicioDAO dao = new EjercicioDAO(mContext);
-                ejercicioInfoList = dao.list();
-            } catch (Exception e) {
-                Log.e(LOG_TAG, "Error durante la carga de los ejercicios.", e);
-            }
-
-            return (Integer) 1;
-        }
-
-        // This method will be executed on the main UI thread and can access the UI and update
-        // the listview. We dismiss the progress dialog after updating the listview.
-        @Override
-        protected void onPostExecute(Integer result) {
-            super.onPostExecute(result);
-            myArrayAdapter = new ArrayAdapter(mContext, ejercicioInfoList);
-            ejerciciosView.setAdapter((ListAdapter) myArrayAdapter);
-            progressDialog.dismiss();
-        }
-
-        // Este metodo es llamado si se cancela el proceso en segundo plano.
-        @Override
-        protected void onCancelled() {
-            super.onCancelled();
-            progressDialog.dismiss();
-        }
     }
 }
